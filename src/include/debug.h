33847bab1cc5f85cf27d86b9660cd52eaa51724b
Pierre Hubert <pierre.git@communiquons.org>
2021-12-11 12:07:15 +0100

    Merge C and H debug files

--- src/include/debug.h
+++ src/include/debug.h
@@ -1,4 +1,6 @@
-#include "stdio.h"
+#include <execinfo.h>
+#include <stdio.h>
+#include <unistd.h>
 
 // #define LOG_DEBUG 1
 
@@ -11,11 +13,25 @@
 #    define LOG(msg) ;
 #endif
 
-void print_backtrace();
+/**
+ * \brief Print current backtrace
+ */
+static inline void print_backtrace()
+{
+    void *buf[50];
+    int num = backtrace(buf, sizeof(buf) / sizeof(void *));
+    backtrace_symbols_fd(buf, num, STDERR_FILENO);
+}
 
 /**
  * \brief Abort execution of program, printing a backtrace
  *
+ * Please use this function only for debugging purposes!
+ *
  * Yanis will kill me when he will see this function
  */
-void abort_with_backtrace();
+static inline void abort_with_backtrace()
+{
+    char *test = NULL;
+    test[0] = 'c';
+}
