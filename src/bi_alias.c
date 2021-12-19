beb685637d34bf8e434bd9aa1840ea832805d4a6
Pierre Hubert <pierre.git@communiquons.org>
2021-12-19 17:20:12 +0100

    Display the list of aliases when alias is called without argument

--- src/bi_alias.c
+++ src/bi_alias.c
@@ -2,6 +2,27 @@
 
 #include "builtins.h"
 
+/** \brief Print a single alias entry */
+static void __print_alias(struct kvpair *pair)
+{
+    printf("%s=%s\n", pair->key, pair->value.word);
+}
+
+/** \brief Show the entire alias available in symtab */
+static void __print_all_aliases(struct symtab *st)
+{
+    for (size_t i = 0; i < st->capacity; i++)
+    {
+        struct kvpair *kv = st->data[i];
+        while (kv)
+        {
+            if (kv->type == KV_ALIAS)
+                __print_alias(kv);
+            kv = kv->next;
+        }
+    }
+}
+
 /**
  * alias builtin
  *
@@ -17,8 +38,8 @@ int bi_alias(char **args, struct symtab *s)
     /* Print aliases list */
     if (*args == NULL)
     {
-        fprintf(stderr, "TODO : print aliases list;\n");
-        return 1;
+        __print_all_aliases(s);
+        return 0;
     }
 
     /* Process alias list */
@@ -34,7 +55,7 @@ int bi_alias(char **args, struct symtab *s)
             if (!pair || pair->type != KV_ALIAS)
                 ret = 1;
             else
-                printf("%s=%s\n", pair->key, pair->value.word);
+                __print_alias(pair);
         }
 
         /* Request to set a new alias */
